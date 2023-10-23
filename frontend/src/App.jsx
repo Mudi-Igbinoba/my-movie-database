a67c518d4a6a118a43f9a76386ce0269fbb7e8f4
Osamudiame Ruth Igbinoba <ruth.igbinoba12@gmail.com>
2023-10-24 00:10:34 +0100

    deleted unnecessary comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,28 @@
-// import { useState } from 'react';
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
-import Hero from './components/Main/HeroPage/Hero';
+import Movies from './components/Main/MoviesPage/Movies';
+import ViewMovie from './components/Main/MoviesPage/ViewMovie';
+import Home from './components/Main/LandingPage/Home';
+import AddMovie from './components/Main/Form/AddMovie';
+import heroBg from './assets/pattern.jpg';
+import Footer from './components/Footer/Footer';
 
 function App() {
     return (
         <div className='font-josefin'>
             <Header />
-            <main>
+            <main
+                className='text-white min-h-[85vh] bg-blend-darken bg-contain bg-center animate-fadeIn bg-black/75'
+                style={{ backgroundImage: `url(${heroBg})` }}>
                 <Routes>
-                    <Route path='/' element={<Hero />} />
+                    <Route path='/' element={<Home />} />
+                    <Route path='/add-movie' element={<AddMovie />} />
+                    <Route path='/movies' element={<Movies />} />
+                    <Route path='/movies/details/:id' element={<ViewMovie />} />
                 </Routes>
             </main>
+            <Footer />
         </div>
     );
 }
